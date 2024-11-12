f41ce9d122ede1ed2f846fcf77c86febc92a4148
TonyCleem <tonycleem@gmail.com>
2024-11-13 00:02:04 +0700

    get salary

--- launch_project_hh_api.py
+++ launch_project_hh_api.py
@@ -1,16 +1,27 @@
 import requests
-import pprint
-
-url = 'https://api.hh.ru/vacancies'
-payload = {'professional_role':'96',
-           'area':'113'}
-
-response = requests.get(url, params=payload)
 
 
-response.raise_for_status()
+programming_languages = ['PHP', 'Ruby', 'Python', 'Java', 'JavaScript']
+found = {}
+for programming_language in programming_languages:
+    url = 'https://api.hh.ru/vacancies'
+    payload = {"text": f"{programming_language}",
+    'area': '1',
+    }
+    
+    response = requests.get(url, params=payload)
+    print(response.url)
+    response.raise_for_status()
+    vacansies = response.json()
+    salares = vacansies['items'][1]['salary']
+    print(salares)
+        # more_salares['from'] = salary['from']
+        # more_salares['to'] = salary['to']
+        # more_salares['currency'] = salary['currency']
+        # more_salares['gross'] = salary['gross']
 
 
-pprint.pprint(response.json())
+
+
 
 
