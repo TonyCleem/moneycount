459bf214a8eb4a93e18fed01ccaf0860d8b48139
TonyCleem <tonycleem@gmail.com>
2024-11-13 21:47:11 +0700

    Add fun get_predict_rub_salary()

--- launch_project_hh_api.py
+++ launch_project_hh_api.py
@@ -1,25 +1,26 @@
 import requests
+import pprint
 
-
-programming_languages = ['PHP', 'Ruby', 'Python', 'Java', 'JavaScript']
-found = {}
-for programming_language in programming_languages:
+def get_predict_rub_salary(vacansies_name):
     url = 'https://api.hh.ru/vacancies'
-    payload = {"text": f"{programming_language}",
-    'area': '1',
-    }
-    
+    payload = {"text": f"{vacansies_name}", 'area': '1',}
     response = requests.get(url, params=payload)
-    print(response.url)
     response.raise_for_status()
-    vacansies = response.json()
-    salares = vacansies['items'][1]['salary']
-    print(salares)
-        # more_salares['from'] = salary['from']
-        # more_salares['to'] = salary['to']
-        # more_salares['currency'] = salary['currency']
-        # more_salares['gross'] = salary['gross']
+    predict_rub_salary = response.json()
+    return predict_rub_salary
+    
 
+if __name__ == '__main__':
+    vacansies_name = 'JavaScript'
+    get_predict_rub_salary = get_predict_rub_salary(vacansies_name)
 
-
-
+    vacancies_salary = get_predict_rub_salary['items']
+    for salary in vacancies_salary:
+        salary_amounts = salary['salary']
+        if not salary_amounts:
+            print(None)
+            continue
+        for salary_amount in salary_amounts.keys():
+            if salary_amount != 'from':
+                continue
+            print(salary_amounts[salary_amount])
