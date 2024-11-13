eb3aa0dcecaaacf49aa1dd42c2ed21a15cc4e9ae
TonyCleem <tonycleem@gmail.com>
2024-11-14 00:02:29 +0700

    fix predict_rub_salary

--- launch_project_hh_api.py
+++ launch_project_hh_api.py
@@ -1,6 +1,7 @@
 import requests
 import pprint
 
+
 def get_rub_salary(vacansies_name):
     url = 'https://api.hh.ru/vacancies'
     payload = {"text": f"{vacansies_name}", 'area': '1',}
@@ -8,6 +9,7 @@ def get_rub_salary(vacansies_name):
     response.raise_for_status()
     rub_salary = response.json()
     return rub_salary
+
 
 def get_vacansies_statistic(vacancies_found, vacancies_processed, average_salary):
     vacansies_statistic = {}
@@ -17,33 +19,51 @@ def get_vacansies_statistic(vacancies_found, vacancies_processed, average_salary
     return vacansies_statistic
 
 
+def get_average_salary(salaries):
+    average_salary = []
+    for salary in salaries:
+        programmer_salary = salary['salary']
+        if not programmer_salary:
+            continue
+        for money in programmer_salary.keys():
+            if money != 'from':
+                continue
+            if not programmer_salary[money]:
+                continue
+            programmer_salary = programmer_salary[money] * 1.2
+            average_salary.append(programmer_salary)
+    average_salary = sum(average_salary) / len(average_salary)
+    average_salary = round(average_salary)
+    return average_salary
+
+
+def get_vacancies_processed(salaries):
+    vacancies_processed = 0
+    for salary in salaries:
+        programmer_salary = salary['salary']
+        if not programmer_salary:
+            continue
+        for money in programmer_salary.keys():
+            if money != 'from':
+                continue
+            vacancies_processed += 1
+    return vacancies_processed
+
+
 if __name__ == '__main__':
     vacansies_name = ['Python', 'Java', 'Ruby', 'PHP']
     predict_rub_salary = {}
     for vacansy in vacansies_name:
         rub_salary = get_rub_salary(vacansy)
-
-        vacancies_processed = 0
-        average_salary = []
+        salaries = rub_salary['items']
 
         vacancies_found = rub_salary['found']
-        salaries = rub_salary['items']
-        for salary in salaries:
-            programmer_salary = salary['salary']
-            if not programmer_salary:
-                continue
-            for money in programmer_salary.keys():
-                if money != 'from':
-                    continue
-                if not programmer_salary[money]:
-                    continue
-                programmer_salary = programmer_salary[money] * 1.2
-                average_salary.append(programmer_salary)
-                vacancies_processed += 1
-        average_salary = sum(average_salary) / len(average_salary)
-        average_salary = round(average_salary)
+        vacancies_processed = get_vacancies_processed(salaries)
+        average_salary = get_average_salary(salaries)
+
         vacansies_statistic = get_vacansies_statistic(vacancies_found, vacancies_processed, average_salary)
         predict_rub_salary[f'{vacansy}'] = vacansies_statistic
+
     pprint.pprint(predict_rub_salary)
 
 
